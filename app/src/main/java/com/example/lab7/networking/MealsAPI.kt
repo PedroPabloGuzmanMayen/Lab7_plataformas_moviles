9c131a74e1a1dd1928d9ff3d0b30179f0293351f
PedroPabloGuzmanMayen <ppguzmanmayen@gmail.com>
2023-09-28 06:34:32 -0600

    API

---
+++ app/src/main/java/com/example/lab7/networking/MealsAPI.kt
@@ -0,0 +1,10 @@
+package com.example.lab7.networking
+
+import com.example.lab7.networking.response.MealsCategoriesResponse
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface MealsApi {
+    @GET("categories.php")
+    fun getMeals(): Call<MealsCategoriesResponse>
+}
