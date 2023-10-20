ce3239f89f7f8a55b80e748777372b8790fa9bb0
PedroPabloGuzmanMayen <ppguzmanmayen@gmail.com>
2023-10-19 21:53:00 -0600

    x

--- app/src/main/java/com/example/lab7/UserInterface/Recipe/View/RecipesView.kt
+++ app/src/main/java/com/example/lab7/UserInterface/Recipe/View/RecipesView.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.Image
 
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.IconButton
@@ -22,6 +23,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
@@ -69,8 +71,9 @@ fun RecipeView(navController: NavController, id : String?){
 
 @Composable
 fun content(arg: Recipe){
-
-    Text(arg.instructions)
+    Text(arg.name, fontSize = 25.sp, textAlign = TextAlign.Center, modifier = Modifier.padding(10.dp))
+    Spacer(modifier = Modifier.padding(10.dp))
+    Text(arg.instructions, fontSize = 15.sp, textAlign = TextAlign.Center, modifier = Modifier.padding(10.dp))
 }
 
 
